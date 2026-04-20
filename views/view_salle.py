0d23cb642655c72dee96c0866a72448bc95b4dc0
malika2714 <malikahaddadi0109@gmail.com>
2026-04-19 23:47:33 -0400

    Creation de la classe ViewSalle

---
+++ views/view_salle.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+from services.service_salle import ServiceSalle
+
+
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.service_salle = ServiceSalle()
+
