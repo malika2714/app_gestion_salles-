83705b7e2b421d9685af3fe791b8309904ac8b45
malika2714 <malikahaddadi0109@gmail.com>
2026-04-19 22:49:42 -0400

    Ajout de la validation des champs obligatoires dans ajouter_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -6,3 +6,9 @@ class ServiceSalle:
 
 def ajouter_salle(self, salle):
     return self.dao_salle.insert_salle(salle)
+
+def ajouter_salle(self, salle):
+    if not salle.code or not salle.libelle or not salle.type or salle.capacite is None:
+        return False, "Tous les champs sont obligatoires."
+
+    return self.dao_salle.insert_salle(salle), "Traitement terminé."
